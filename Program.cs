eb5c5c48482ff50bbd2a556b198c5635a1a51568
Foster90 <matthew.foster90@hotmail.com>
2020-10-21 16:05:41 +0100

    Updated with time automation

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using TweetSharp;
 
 namespace TwitterBot
@@ -17,25 +21,48 @@ namespace TwitterBot
         private static string accesstoken = ConfigurationManager.AppSettings.Get("accesstoken");
         private static string accesstokensecert = ConfigurationManager.AppSettings.Get("accesstokensecert");
         static readonly Random rnd = new Random();
-     
-        
+        private static System.Timers.Timer aTimer;
+        private static ManualResetEvent mre = new ManualResetEvent(false);
+        public static List<Quote> qlist = new List<Quote>();
+        public static int quotecount;
+        public static bool listempty = false;
+
 
         private static TwitterService service = new TwitterService(apikey, apisercetkey, accesstoken, accesstokensecert); 
 
         static void Main(string[] args)
 
         {
-            DateTime timeNow = DateTime.Now;
-            DateTime timeWeek = DateTime.Now.AddMinutes(5);
-            DateTime randomdate = GetRandomDate(timeNow, timeWeek);
 
+            string json = File.ReadAllText("../../datafile.json");
+            qlist = JsonConvert.DeserializeObject<List<Quote>>(json);
+            string list = qlist[quotecount].qutoe;
 
+            while (listempty == false)
+            {
 
-            Console.WriteLine($"<{DateTime.Now}> - Bot Started");
+                DateTime timeNow = DateTime.Now;
+                DateTime timeWeek = DateTime.Now.AddMinutes(10);
+                DateTime randomdate = GetRandomDate(timeNow, timeWeek);
 
-            SendTweet("It's just a hairy turkey!"); 
-            Console.ReadLine();
+                double inter = (randomdate - timeNow).TotalMilliseconds;
 
+                Console.WriteLine($"<{DateTime.Now}> - Bot Started");
+                Console.WriteLine(timeNow);
+                Console.WriteLine(timeWeek);
+                Console.WriteLine(randomdate);
+
+                SetTimer(inter, list);
+                mre.Reset();
+
+           
+
+               
+                Console.ReadLine();
+
+                aTimer.Stop();
+                aTimer.Dispose();
+            }
         }
 
         private static void SendTweet(string _status)
@@ -70,5 +97,50 @@ namespace TwitterBot
             return from + randTimeSpan;
         }
 
+
+
+        private static void SetTimer(double time, string quote)
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(time);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = false;
+            aTimer.Enabled = true;
+            mre.WaitOne();
+
+
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                               e.SignalTime);
+            string quote = GetQuote(quotecount);
+
+            Console.WriteLine("{0}", quote);
+            SendTweet(quote);
+
+            
+            mre.Set();
+            quotecount++;
+
+            //aTimer.Elapsed -= OnTimedEvent;
+
+        }
+
+        private static string GetQuote(int qnumber)
+        {
+            if (qnumber + 1 == qlist.Count())
+            {
+                listempty = true;
+                return qlist[qnumber].qutoe;
+
+            }
+            else
+            {
+                return qlist[qnumber].qutoe;
+            }
+        }
     }
 }
---
+++ Quote.cs
@@ -0,0 +1,14 @@
+﻿namespace TwitterBot
+{
+    public class Quote
+    {
+
+        public string qutoe { get; set; }
+
+
+        public Quote(string x)
+        {
+            qutoe = x;
+        }
+    }
+}
